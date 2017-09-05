991bd1b57e899e76fd4a10446a5c7cb0f90f111d
sjtg <chipilej@gmail.com>
2017-09-05 21:39:50 +0200

    Added new views and created forms

--- eressite/eresapp/views.py
+++ eressite/eresapp/views.py
@@ -6,8 +6,29 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+from .forms import PostForm
+
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'site/post_list.html', {'posts': posts})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(request.POST, instance=post)
+    return render(request, 'eresapp/post_edit.html', {'form': form})
