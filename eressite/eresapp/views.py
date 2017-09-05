46a69f76d9f685d3bbbe7cb8be308c466e24cfe5
sjtg <chipilej@gmail.com>
2017-09-05 23:34:52 +0200

    eresapp to site

--- eressite/eresapp/views.py
+++ eressite/eresapp/views.py
@@ -16,11 +16,11 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'site/post_detail.html', {'post': post})
 
 def post_new(request):
     form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'site/post_edit.html', {'form': form})
 
 
 def post_edit(request, pk):
@@ -35,4 +35,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(request.POST, instance=post)
-    return render(request, 'eresapp/post_edit.html', {'form': form})
+    return render(request, 'site/post_edit.html', {'form': form})
