4836f4308fe7e153cd0b16140fb5e18fb176bb75
sjtg <chipilej@gmail.com>
2017-09-22 20:40:32 +0200

    modified

--- eressite/eresapp/urls.py
+++ eressite/eresapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from . import views as core_views
 
 
 
@@ -9,4 +10,5 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^upload/$', views.UploadView.as_view(), name='upload'),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
--- eressite/eresapp/views.py
+++ eressite/eresapp/views.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import render, redirect, HttpResponse
 
 from django.utils import timezone
 from .models import Post
@@ -16,6 +20,22 @@ from .models import Photo
 
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form  = UserCreationForm(request.POST)
+	if form.is_valid():
+ 	    form.save()
+	    username = form.cleaned_data.get('username')
+	    raw_passwprd = form.cleaned_data.get('password')
+	    user = authenticate(username=username, password=raw_password)
+	    login(request, user)
+	    return redirect('home')
+	else:
+	    form = UserCreationForm()
+
+	return render(request, 'site/signup.html', {'form':form})
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
