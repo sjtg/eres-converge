976ea19897c646db3892d63495f1835ada83687c
sjtg <chipilej@gmail.com>
2017-09-26 12:16:38 +0200

    1)Bug Fixed - Able to upload documents to the system.

--- eressite/eressite/settings.py
+++ eressite/eressite/settings.py
@@ -124,5 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = 'student'
+LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'home'
