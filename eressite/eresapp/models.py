3d0316b8172dba522a907224078d7f3bab14ea45
sjtg <chipilej@gmail.com>
2017-09-28 16:04:15 +0200

    1)Added user authentication on the base2.html

--- eressite/eresapp/models.py
+++ eressite/eresapp/models.py
@@ -45,6 +45,7 @@ class Post(models.Model):
 
 
 class Photo(models.Model):
+    user_upload = Post.author
     title = models.CharField(max_length=255, blank=True)
     file = models.FileField(upload_to='photos/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
