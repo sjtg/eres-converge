f4cdbae5649e35673bd7d85f61b0e49786f9b7e7
sjtg <chipilej@gmail.com>
2017-10-14 11:36:55 +0200

    1)Modified index page 2)Modified Notice baord

--- eressite/eresapp/views.py
+++ eressite/eresapp/views.py
@@ -39,7 +39,7 @@ def signup(request):
 		    raw_password = form.cleaned_data.get('password1')
 		    user = authenticate(username=username, password=raw_password)
 		    login(request, user)
-		    return redirect('student')
+		    return redirect('dashboard')
 	else:
 	    form = SignUpForm()
 	return render(request, 'site/signup.html', {'form':form})
